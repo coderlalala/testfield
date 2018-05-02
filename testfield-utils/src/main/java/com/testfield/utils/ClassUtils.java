5e29865c14a2a2510f46dec219a518eee893bec1
Administrator <Administrator@HCDL180208-PC>
2018-05-02 16:49:53 +0800

    update

--- testfield-utils/src/main/java/com/testfield/utils/ClassUtils.java
+++ testfield-utils/src/main/java/com/testfield/utils/ClassUtils.java
@@ -1,19 +1,26 @@
 package com.testfield.utils;
 
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class ClassUtils {
 
-    public static <V, T extends Serializable> V convert(T source, V target) {
+    public static <V, T extends Serializable> V convert(T source, Class<V> target) throws Exception {
 
         if (source == null)
             throw new NullPointerException("ClassUtils.convert source is null error!");
         if (target == null)
             throw new NullPointerException("ClassUtils.convert target is null error!");
 
-        Class<? extends Object> targetClass = target.getClass();
-        Method[] targetMethods = targetClass.getMethods();
+        Constructor<? extends V> con = target.getConstructor();
+        V returnObj = (V) con.newInstance();
+
+        Method[] targetMethods = target.getMethods();
+        if (targetMethods == null || targetMethods.length == 0) {
+            return returnObj;
+        }
+
         if (targetMethods != null && targetMethods.length != 0) {
 
         }
