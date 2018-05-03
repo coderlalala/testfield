794652cbd67272f7120eb1fd94d1d7ef076bd8c9
Administrator <Administrator@HCDL180208-PC>
2018-05-03 16:23:06 +0800

    update

--- testfield-utils/src/main/java/com/testfield/utils/StringUtils.java
+++ testfield-utils/src/main/java/com/testfield/utils/StringUtils.java
@@ -6,7 +6,15 @@ public class StringUtils {
         return s == null || s.length() == 0;
     }
 
+    public static boolean isNotEmpty(String s) {
+        return s != null && s.length() > 0;
+    }
+
     public static boolean isBlank(String s) {
         return isEmpty(s) || s.trim().length() == 0;
+    }
+
+    public static boolean isNotBlank(String s) {
+        return isNotEmpty(s) && s.trim().length() > 0;
     }
 }
